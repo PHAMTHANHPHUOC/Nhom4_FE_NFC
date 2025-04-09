f9f957524e494a62cce9f2ed1ff9f5a831e49f63
Phạm Thanh Phước <thanhphuocj3@gmail.com>
2025-04-09 15:11:54 +0700

    update profile fix

--- src/layout/components/TopClient.vue
+++ src/layout/components/TopClient.vue
@@ -5,8 +5,11 @@
         <!-- Logo Section -->
         <div class="topbar-logo-header d-flex align-items-center">
           <div class="logo-container">
-            <img src="https://posapp.vn/wp-content/themes/twentysixteen-child/images/icon-user-slide.png"
-              class="logo-icon" alt="logo icon" />
+            <img
+              src="https://posapp.vn/wp-content/themes/twentysixteen-child/images/icon-user-slide.png"
+              class="logo-icon"
+              alt="logo icon"
+            />
           </div>
           <div class="logo-text-container">
             <h4 class="logo-text">Quản Lý Nhân Sự</h4>
@@ -21,11 +24,19 @@
         <!-- Search Bar -->
         <div class="search-bar flex-grow-1">
           <div class="position-relative search-bar-box">
-            <input type="text" class="form-control search-control" placeholder="Tìm kiếm..." />
-            <span class="position-absolute top-50 search-show translate-middle-y">
+            <input
+              type="text"
+              class="form-control search-control"
+              placeholder="Tìm kiếm..."
+            />
+            <span
+              class="position-absolute top-50 search-show translate-middle-y"
+            >
               <i class="bx bx-search"></i>
             </span>
-            <span class="position-absolute top-50 search-close translate-middle-y">
+            <span
+              class="position-absolute top-50 search-close translate-middle-y"
+            >
               <i class="bx bx-x"></i>
             </span>
           </div>
@@ -40,8 +51,13 @@
               </a>
             </li>
             <li class="nav-item dropdown dropdown-large">
-              <a class="nav-link dropdown-toggle dropdown-toggle-nocaret" href="#" role="button"
-                data-bs-toggle="dropdown" aria-expanded="false">
+              <a
+                class="nav-link dropdown-toggle dropdown-toggle-nocaret"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
                 <i class="bx bx-category"></i>
               </a>
               <div class="dropdown-menu dropdown-menu-end">
@@ -72,16 +88,29 @@
 
         <!-- User Profile -->
         <div class="user-box dropdown">
-          <a class="d-flex align-items-center nav-link dropdown-toggle dropdown-toggle-nocaret" href="#" role="button"
-            data-bs-toggle="dropdown" aria-expanded="false">
-            <img src="../../assets/images/avatars/avatar-2.png" class="user-img" alt="user avatar" />
+          <a
+            class="d-flex align-items-center nav-link dropdown-toggle dropdown-toggle-nocaret"
+            href="#"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            <img
+              src="../../assets/images/avatars/avatar-2.png"
+              class="user-img"
+              alt="user avatar"
+            />
             <div class="user-info ps-3">
               <p class="user-name mb-0">Tên Nhân Viên</p>
             </div>
           </a>
           <ul class="dropdown-menu dropdown-menu-end">
             <li>
-              <router-link :to="`/nhan-vien/ho-so/${id}`" class="dropdown-item">
+              <router-link
+                :to="`/nhan-vien/ho-so/${id}`"
+                class="dropdown-item"
+                v-on:click="alert(this.id)"
+              >
                 <i class="bx bx-user"></i>
                 <span>Thông tin cá nhân</span>
               </router-link>
@@ -90,7 +119,11 @@
               <div class="dropdown-divider mb-0"></div>
             </li>
             <li>
-              <a v-on:click="dangXuat()" class="dropdown-item" href="javascript:;">
+              <a
+                v-on:click="dangXuat()"
+                class="dropdown-item"
+                href="javascript:;"
+              >
                 <i class="bx bx-log-out"></i>
                 <span>Đăng xuất</span>
               </a>
@@ -112,13 +145,10 @@ export default {
   data() {
     return {
       id: JSON.parse(localStorage.getItem("data"))?.id || null,
-      profile: {
-      },
-    }
+      profile: {},
+    };
   },
-  mounted() {
-
-  },
+  mounted() {},
   methods: {
     dangXuat() {
       baseRequest.get("dang-xuat").then((res) => {
@@ -128,9 +158,8 @@ export default {
           toaster.success(res.data.message);
           this.$router.push("/quan-ly/dang-nhap");
         }
-      })
-    }
-
+      });
+    },
   },
 };
 </script>
