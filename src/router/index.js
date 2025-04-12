d7e8fdf5e592192015fd0e36e07e2b04ae67d5fb
Phạm Thanh Phước <thanhphuocj3@gmail.com>
2025-04-12 12:55:49 +0700

    update-DangNhap-NhanVien

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,33 @@
 import { createRouter, createWebHistory } from "vue-router"; // cài vue-router: npm install vue-router@next --save
 import checkLogin from "./checkLogin";
 
-const routes = [];
+const routes = [
+  {
+    path: "/",
+    component: () => import("../layout/wrapper/index.vue"),
+    beforeEnter: checkLogin,
+  },
+  {
+    path: "/admin/dang-nhap",
+    component: () => import("../components/Admin/DangNhap/index.vue"),
+    meta: { layout: "blank" },
+  },
+  {
+    path: "/admin/phong-ban",
+    component: () => import("../components/Admin/PhongBan/index.vue"),
+    beforeEnter: checkLogin,
+  },
+  {
+    path: "/admin/chuc-vu",
+    component: () => import("../components/Admin/ChucVu/index.vue"),
+    beforeEnter: checkLogin,
+  },
+  {
+    path: "/admin/nhan-vien",
+    component: () => import("../components/Admin/NhanVien/index.vue"),
+    beforeEnter: checkLogin,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
